319d94c6cd873162b6fbcc976e4ea0290238ba03
Ashwini <ashwini7amin@gmail.com>
2025-12-08 18:02:19 +0530

    client Certification

---
+++ MvenDec5ClientCertification/src/main/java/com/mphasis/ClientCertServlet.java
@@ -0,0 +1,51 @@
+package com.mphasis;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.cert.X509Certificate;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class ClientCertServlet
+ */
+@WebServlet("/ClientCertServlet")
+public class ClientCertServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ClientCertServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		X509Certificate[] certs=(X509Certificate[] )request.getAttribute("jakarta.servlet.request.X509Certificate");
+		PrintWriter out=response.getWriter();
+		out.println("<h1>Client Certification Model of Security</h1>");
+		if(certs!=null)
+		{
+			out.println("<h3>Subject:</h3>"+certs[0].getSubjectDN().getName());
+			                                                                                                           
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
